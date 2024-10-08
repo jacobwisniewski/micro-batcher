5f29142b51d53b8470a497adabbd356041176514
Jacob Wisniewski <jacob.wisniewski@rea-group.com>
2024-10-08 21:34:25 +1100

    quick dirty draft

--- src/MicroBatcher.ts
+++ src/MicroBatcher.ts
@@ -1,21 +1,16 @@
 type Job = () => any;
-type JobResult = any;
+type JobResult = JobSuccess | JobFailure;
+type JobSuccess = { status: 'success'; result: any };
+type JobFailure = { status: 'failure'; error: Error };
 type BatchProcessor = (jobs: Job[]) => Promise<JobResult[]>;
 
-interface MicroBatcher {
-  submitJob: (job: Job) => Promise<JobResult>;
-  shutdown: () => Promise<void>;
-}
-
-// MicroBatcher accepts the following parameters
-// - processor: The function that processes the jobs
-// - batchSize: The maximum number of jobs to process in a single batch
-// - interval: The maximum time to wait before processing a batch
-class MicroBatcher implements MicroBatcher {
+class MicroBatcher {
   private batchSize: number
   private batchFrequency: number
   private batchProcessor: BatchProcessor
-  private jobQeueue: Job[] = []
+  private jobQueue:  { job: Job; resolve: (result: JobResult) => void; reject: (error: Error) => void }[] = []
+  private intervalId: number;
+
 
   constructor(
     processor: BatchProcessor,
@@ -25,9 +20,45 @@ class MicroBatcher implements MicroBatcher {
     this.batchProcessor = processor;
     this.batchSize = batchSize;
     this.batchFrequency = batchFrequency;
+
+    this.intervalId = setInterval(() => this.processBatch(), this.batchFrequency);
   }
 
-  public submitJob(job: Job): Promise<JobResult> {}
+  public async submitJob(job: Job): Promise<JobResult> {
+    return new Promise((resolve, reject) => {
+      this.jobQueue.push({ job, resolve, reject });
 
-  public shutdown(): Promise<void> {}
+      if (this.jobQueue.length >= this.batchSize) {
+        this.processBatch();
+      }
+    });
+
+  }
+
+  public async shutdown(): Promise<void> {
+    clearInterval(this.intervalId);
+    while (this.jobQueue.length > 0) {
+      await this.processBatch();
+    }
+  }
+
+  private async processBatch(): Promise<void> {
+    if (this.jobQueue.length === 0) return;
+
+    const jobsToProcess = this.jobQueue.splice(0, this.batchSize)
+    try {
+      const jobResults = await this.batchProcessor(jobsToProcess.map(({job}) => job))
+
+      jobResults.forEach((jobResult, i) => {
+        const {resolve, reject} = jobsToProcess[i]
+        if (jobResult.status === 'success') {
+          resolve(jobResult)
+        } else {
+          reject(jobResult.error)
+        }
+      })
+    } catch (error) {
+      jobsToProcess.forEach(({reject}) => reject(error))
+    }
+  }
 }
